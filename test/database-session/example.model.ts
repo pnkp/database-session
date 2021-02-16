e03b7830a3da4524cb611e864545b4e17fa7dc1c
pantyporowicz <p.antyporowicz@merixstudio.com>
2021-02-16 20:15:27 +0100

    Added testing module

---
+++ test/database-session/example.model.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ExampleModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  value: string;
+
+  constructor(init: Partial<ExampleModel>) {
+    Object.assign(this as ExampleModel, init);
+  }
+}
---
+++ test/database-session/testTransaction.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Post } from "@nestjs/common";
+import { ExampleRepository } from "./example.repository";
+import { DatabaseSession, InjectDatabaseSession } from "../../libs/database-session/src";
+
+@Controller('transactions')
+export class TestTransactionController {
+
+  constructor(
+    private readonly exampleRepository: ExampleRepository,
+    @InjectDatabaseSession()
+    private readonly databaseSession: DatabaseSession
+    ) {
+  }
+
+  @Post()
+  async commitTransaction(
+    @Body() data: any,
+  ): Promise<any> {
+    try {
+      await this.databaseSession.transactionStart()
+      const result = await this.exampleRepository.save(data);
+      await this.databaseSession.transactionCommit();
+      return result;
+    } catch (e) {
+      await this.databaseSession.transactionRollback();
+      throw e;
+    }
+  }
+
+  @Delete()
+  async rollbackTransaction(
+    @Body() data: any,
+  ): Promise<any> {
+    try {
+      await this.databaseSession.transactionStart()
+      const result = await this.exampleRepository.save(data);
+      throw new Error("Transaction will be rollback!")
+      await this.databaseSession.transactionCommit();
+      return result;
+    } catch (e) {
+      await this.databaseSession.transactionRollback();
+      throw e;
+    }
+  }
+}
