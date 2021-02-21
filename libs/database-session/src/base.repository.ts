4eb097ecbd8b28a1bc28cd2fe37bdf6e9b79e288
pantyporowicz <p.antyporowicz@merixstudio.com>
2021-02-21 11:53:00 +0100

    added factory for database module

--- libs/database-session/src/base.repository.ts
+++ libs/database-session/src/base.repository.ts
@@ -1,17 +1,16 @@
 import { EntityManager, Repository } from "typeorm";
 import { EntityTarget } from "typeorm/common/EntityTarget";
-import { SessionEntityManager } from "./session.entity-manager";
 
 export abstract class BaseRepository {
   protected constructor(
-    private readonly databaseSession: SessionEntityManager) {
-  }
+    private readonly sessionEntityManager: EntityManager,
+  ) {}
 
   protected getRepository<TEntity>(entity: EntityTarget<TEntity>): Repository<TEntity> {
-    return this.databaseSession.getEntityManager().getRepository(entity)
+    return this.sessionEntityManager.getRepository(entity)
   }
 
   protected getEntityManager(): EntityManager {
-    return this.databaseSession.getEntityManager();
+    return this.sessionEntityManager;
   }
 }
--- libs/database-session/src/database-session.module.ts
+++ libs/database-session/src/database-session.module.ts
@@ -1,32 +1,47 @@
-import { DynamicModule, Module, Scope } from '@nestjs/common';
+import { DynamicModule, Global, Module, Scope } from '@nestjs/common';
 import { TypeOrmDatabaseSession } from './type-orm.database-session';
-import { DATABASE_SESSION, InjectDatabaseSession, SESSION_ENTITY_MANAGER } from "./inject-decorators";
-import { Connection, ConnectionOptions } from "typeorm";
-import { getConnectionToken } from "@nestjs/typeorm";
+import { DATABASE_SESSION, SESSION_ENTITY_MANAGER } from "./inject-decorators";
+import { Connection } from "typeorm";
+import { FactoryProvider } from "@nestjs/common/interfaces/modules/provider.interface";
 
+@Global()
 @Module({})
 export class DatabaseSessionModule {
-  static forRoot(connectionName?: string): DynamicModule {
+  static forRootAsync(factory: DatabaseSessionModuleOptions): DynamicModule {
+    return DatabaseSessionModule.forRoot(factory);
+  }
+
+  private static forRoot(factory: DatabaseSessionModuleOptions): DynamicModule {
     return {
       providers: [
         {
+          useFactory: factory.useFactory,
+          inject: factory.inject,
+          provide: 'DatabaseSessionOptions'
+        },
+        {
           provide: DATABASE_SESSION,
-          useFactory: (connection: Connection) => {
+          useFactory: async (connection: Connection) => {
             return new TypeOrmDatabaseSession(connection);
           },
           scope: Scope.REQUEST,
-          inject: [getConnectionToken(connectionName)]
+          inject: ['DatabaseSessionOptions'],
         },
         {
           provide: SESSION_ENTITY_MANAGER,
           useFactory: (typeOrmDatabaseSession: TypeOrmDatabaseSession) => {
-            return typeOrmDatabaseSession;
+            return typeOrmDatabaseSession.getEntityManager();
           },
           inject: [DATABASE_SESSION]
         }
       ],
       exports: [DATABASE_SESSION, SESSION_ENTITY_MANAGER],
+      imports: factory.imports,
       module: DatabaseSessionModule,
     };
   }
+}
+
+export interface DatabaseSessionModuleOptions extends Omit<FactoryProvider<Promise<Connection>>, "provide" | "scope"> {
+  imports?: DynamicModule[];
 }
