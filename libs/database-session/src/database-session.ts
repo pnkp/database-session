b6ef4b1e95ea7f0a1111aa5c22652d38510ad365
pantyporowicz <p.antyporowicz@merixstudio.com>
2021-02-28 21:51:24 +0100

    cleaned up

--- libs/database-session/src/database-session.ts
+++ libs/database-session/src/database-session.ts
@@ -1,5 +1,9 @@
+import { Repository } from "typeorm";
+import { EntityTarget } from "typeorm/common/EntityTarget";
+
 export interface DatabaseSession {
   transactionStart(): Promise<void>;
   transactionCommit(): Promise<void>;
   transactionRollback(): Promise<void>;
+  getRepository<TEntity>(entity: EntityTarget<TEntity>): Repository<TEntity>;
 }
